5cc48aa1cf682bfeb723419f85d03568415f280a
Robbai <ralbanese18@gmail.com>
2020-10-23 18:54:31 +0200

    Assignment 1 + Tutorial

---
+++ Assignment_1/Assignment_1.pml
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="Assignment_1" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="Sport" src="Sport/Sport.dlg" />
+        <Dialog name="Food" src="Food/Food.dlg" />
+        <Dialog name="Animals" src="Animals/Animals.dlg" />
+    </Dialogs>
+    <Resources />
+    <Topics>
+        <Topic name="Sport_enu" src="Sport/Sport_enu.top" topicName="Sport" language="en_US" />
+        <Topic name="Food_enu" src="Food/Food_enu.top" topicName="Food" language="en_US" />
+        <Topic name="Animals_enu" src="Animals/Animals_enu.top" topicName="Animals" language="en_US" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ Assignment_1/translations/translation_en_US.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Fear/Fear1/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You are scaring me</source>
+            <comment>Text</comment>
+            <translation type="unfinished">You are scaring me</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Fear/Fear2/Say</name>
+        <message>
+            <source>I'm terrified</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I'm terrified</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Fear/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Fear1/Say</name>
+        <message>
+            <source>You are scaring me</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You are scaring me</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>You are scaring me</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You are scaring me</translation>
+        </message>
+    </context>
+</TS>
